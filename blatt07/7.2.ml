922f48a9639c4840387b51161b491b45e06384ca
Mathias Jackermeier <mathiasjackermeier@hotmail.de>
2017-12-09 13:20:47 +0100

    added my own solutions to the exercises

--- blatt07/7.2.ml
+++ blatt07/7.2.ml
@@ -12,3 +12,19 @@ let rec fold_right f = function (*not end-recursive*)
 let sum = fold_left (+) 0;;
 let map f xs = fold_right (fun x a -> f x ::a) xs [];; (* (f x)::a *)
 let map_rev f = fold_left (fun a x -> f x ::a) [];;
+
+(* here are my own solutions (implemented myself) *)
+let rec fold_left f z l = match l with
+  | [] -> z
+  | x::xs -> fold_left f (f z x) xs
+
+let rec fold_right f l z = match l with
+  | [] -> z
+  | x::xs -> f x (fold_right f xs z)
+
+let sum l = fold_left (+) 0 l
+
+let map f l = fold_right (fun a b -> (f a)::b) l []
+
+let map_rev f l = fold_left (fun a b -> (f b)::a) [] l
+
